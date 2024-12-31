9fe8e43f391b6f868b0bf2b97b070dc9710acf4a
TcarMolod <chukingeorgiy@gmail.com>
2024-12-31 16:23:43 +0500

    Работа в main

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,21 @@
-export const checkStringLength = (String, length) => {
-  if (length >= String.length) {
-    return true;
+/*const checkStringLength = (str, length) => str.length <= length;
+
+console.log(checkStringLength('Kalinka', 2));
+console.log(checkStringLength('Ekaterinburg', 15));
+console.log(checkStringLength('Pohod', 5));
+
+function checkPalindrome (str) {
+  const string = str.replaceAll(' ', '').toLowerCase();
+  for (let i = 0; i < string.length / 2; i++) {
+    if (string.at(i) !== string.at(-i - 1)) {
+      return false;
+    }
   }
-  return false;
-};
+  return true;
+}
+
+console.log(checkPalindrome('Kalinka'));
+console.log(checkPalindrome('Paloma'));
+console.log(checkPalindrome('Kayak'));
+console.log(checkPalindrome('Лёша на полке клопа нашёл '));
+*/
